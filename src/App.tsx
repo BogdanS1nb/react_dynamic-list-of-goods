0270b572140229949eada3c71b99f616e23796b7
Bogdan Kalytovskiy <bogdankalytovskkiy@gmail.com>
2025-08-03 19:36:56 +0300

    add1

--- src/App.tsx
+++ src/App.tsx
@@ -9,38 +9,60 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const loadAllGoods = async () => {
+    setIsLoading(true);
     const allGoods = await getAll();
 
     setGoods(allGoods);
+    setIsLoading(false);
   };
 
   const load5First = async () => {
+    setIsLoading(true);
     const top5 = await get5First();
 
     setGoods(top5);
+    setIsLoading(false);
   };
 
   const loadRed = async () => {
+    setIsLoading(true);
     const red = await getRedGoods();
 
     setGoods(red);
+    setIsLoading(false);
   };
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
 
-      <button type="button" data-cy="all-button" onClick={loadAllGoods}>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={loadAllGoods}
+        disabled={isLoading}
+      >
         Load all goods
       </button>
 
-      <button type="button" data-cy="first-five-button" onClick={load5First}>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={load5First}
+        disabled={isLoading}
+      >
         Load 5 first goods
       </button>
 
-      <button type="button" data-cy="red-button" onClick={loadRed}>
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={loadRed}
+        disabled={isLoading}
+      >
         Load red goods
       </button>
 
